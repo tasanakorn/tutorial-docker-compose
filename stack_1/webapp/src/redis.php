fe59e4b731f4959a5b31b4b0db0cd9819e958562
Tasanakorn P <tasanakorn@gmail.com>
2018-06-24 12:21:26 +0700

    Init

---
+++ stack_1/webapp/src/redis.php
@@ -0,0 +1,15 @@
+<?php
+    header('Content-Type: application/json');
+    $redis = new Redis();
+    $redis->pconnect('redis', 6379);
+    if (isset($_GET['action']) && ($_GET['action'] == 'set')) {
+        $redis->set("message", $_GET['message']);
+        echo json_encode(array(
+            "action" => "set"
+        ));
+    } else {
+        echo json_encode(array(
+            "action" => "get",
+            "message" => $redis->get("message")
+        ));
+    }
