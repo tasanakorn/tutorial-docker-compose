f5f78735250a858cd819a258ebb7412f5ab25f33
Tasanakorn P <tasanakorn@gmail.com>
2018-06-26 20:57:22 +0700

    Remove links, shorten code

--- stack_1/webapp/src/redis.php
+++ stack_1/webapp/src/redis.php
@@ -4,12 +4,12 @@
     $redis->pconnect('redis', 6379);
     if (isset($_GET['action']) && ($_GET['action'] == 'set')) {
         $redis->set("message", $_GET['message']);
-        echo json_encode(array(
+        echo json_encode([
             "action" => "set"
-        ));
+        ]);
     } else {
-        echo json_encode(array(
+        echo json_encode([
             "action" => "get",
             "message" => $redis->get("message")
-        ));
+        ]);
     }
